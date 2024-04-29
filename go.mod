454d5d4b8228c637f5804881acf2ecd5906079b1
tingkatin <muhammad.nabil@superindo.co.id>
2024-04-29 15:17:55 +0700

    init app

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mnabil1718/go-module-app.git
+
+go 1.22.0
+
+require github.com/mnabil1718/go-module-starter v0.1.0
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import sayHello "github.com/mnabil1718/go-module-starter"
+
+func main() {
+
+	sayHello.SayHello()
+
+}
